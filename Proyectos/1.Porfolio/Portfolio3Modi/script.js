e257b2cd088e44d30ecd61fc3770db1c842819da
unknown <poncefernandoandres@gmail.com>
2024-10-29 22:42:34 -0300

    add

---
+++ Proyectos/1.Porfolio/Portfolio3Modi/script.js
@@ -0,0 +1,28 @@
+// esto nos permitiria que el header se mantenga fluido en el scroleo y ademas le podemos dar propiedades al sticky
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {header.classList.toggle("sticky", window.scrollY > 130);
+});
+
+
+let menu = document.querySelector('#menu-ico');
+let menulist = document.querySelector('.menulist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menulist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    menulist.classList.remove('open');
+};
+
+
+var typed = new Typed(".input", {
+    strings: ["Web Designer.", "Web Developer.", "Photographer."],
+    backSpeed: 80,        // La velocidad a la que se borra el texto
+    typeSpeed: 120,       // La velocidad a la que se escribe el texto (ajusta este valor para ralentizar la escritura)
+    loop: true,
+});
